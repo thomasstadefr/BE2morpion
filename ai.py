248faff1b28c88c971a0e586143f133f089b42f4
tguyonva <thomas.guyonvarch@ec-lyon.fr>
2024-10-31 09:33:51 +0100

    correction qq bugs

--- ai.py
+++ ai.py
@@ -1,9 +1,33 @@
 from morpion import*
 
+def lst_allowed_moves(G, is_new):
+    n = G.size
+    c = G.turn
+    res = []
+
+    if not(is_new):
+        lst_pawns = []
+        for i in range(n):
+            for j in range(n):
+                if G.board[i][j] == c:
+                    lst_pawns.append((i, j))
+        
+        for i in range(n):
+            for j in range(n):
+                if G.board[i][j] == 0:
+                    for k in range(n):
+                        res.append(Move(c, (i, j), lst_pawns[k]))
+
+    else:
+        for i in range(n):
+            for j in range(n):
+                if G.board[i][j] == 0:
+                    res.append(Move(c, (i, j), None))
+
+    return res
+
 def ai_move(G, is_new):
-    if is_new:
-        return Move(G.turn, (int(input("y final : ")), int(input("x final : "))), None)
-    return Move(G.turn, (int(input("y final : ")), int(input("x final : "))), (int(input("y init : ")), int(input("x init : "))))
+    return lst_allowed_moves(G, is_new)[0]
 
 def make_move_ai(G):
     G.play_move(ai_move(G, G.played_moves < G.size))
